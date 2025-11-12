d577c34b25f8d4e611cf8e0555706a24f799c942
Jayashree0507 <jayashrees0507@gmail.com>
2025-11-12 09:40:31 +0530

    second push

---
+++ padi-now/app/component/chatarea.tsx
@@ -0,0 +1,8 @@
+// components/ChatArea.tsx
+export default function ChatArea() {
+  return (
+    <div className="flex-1 bg-gray-300 flex flex-col justify-between p-6">
+      <div className="flex-1 overflow-y-auto"></div>
+    </div>
+  );
+}
