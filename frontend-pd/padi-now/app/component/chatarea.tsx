d5a1ccb8482b039523a345852f70ca1da9f864f8
Rallin-ja <dukerallin2004@gmail.com>
2025-11-16 07:26:45 +0530

    final update

--- frontend-pd/padi-now/app/component/chatarea.tsx
+++
@@ -1,8 +0,0 @@
-// components/ChatArea.tsx
-export default function ChatArea() {
-  return (
-    <div className="flex-1 bg-gray-300 flex flex-col justify-between p-6">
-      <div className="flex-1 overflow-y-auto"></div>
-    </div>
-  );
-}
